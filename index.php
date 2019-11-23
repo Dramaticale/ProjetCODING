210d43e23244ae570ed9b05f2e7db18e3ae181a8
Matthieu <guertener.matthieu@orange.fr>
2019-11-23 10:48:42 +0100

    Init projet CODING.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Projet YSCHOOLS fin d'année">
+    <link rel="stylesheet" href="css/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projet Yschools - Jeu de Rôles</title>
+</head>
+<body>
+    <header>
+        <div class="topbanniere">
+            <img src="MotifBanniere.png">
+        </div>
+    </header>
+<?php include("navbar.php")?>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="index.php"><img src="img/logo accueil.png" width="50" height="30"></a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Démarrer une partie</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Continuer une partie</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Forum</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Index</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div class="connexion">
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="text" placeholder="Identifiant">
+                <input class="form-control mr-sm-2" type="password" placeholder="Mot de passe">
+                <button class="btn btn-primary" type="submit">Se connecter</button>
+            </form>
+            <div class="inscription" style="color: white">
+                Pas de compte ? <a href="inscription.php">S'enregistrer</a>
+            </div>
+        </div>
+</nav>

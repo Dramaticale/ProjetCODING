d8c29800916911a730f729e4982554b9e9024525
Matthieu <guertener.matthieu@orange.fr>
2019-12-09 15:43:40 +0100

    tout

--- Ressources/Commun/navbar.php
+++ Ressources/Commun/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg">
-        <a class="nav-link" href="../../Accueil/index.php">Accueil</a>
+        <a class="nav-link" href="index.php">Accueil</a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">

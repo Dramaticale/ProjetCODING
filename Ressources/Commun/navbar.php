6eb6e1e897769fb755a0ebff5603676485869c3b
Matthieu <guertener.matthieu@orange.fr>
2019-12-09 15:49:26 +0100

    navbar

--- Ressources/Commun/navbar.php
+++ Ressources/Commun/navbar.php
@@ -1,19 +1,24 @@
+<html>
+<head>
+    <link rel="stylesheet" href="../../Ressources/Commun/style.css"
+</head>
 <nav class="navbar navbar-expand-lg">
-        <a class="nav-link" href="index.php">Accueil</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+    <a class="nav-link" href="index.php">Accueil</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <a class="nav-link" href="#">Démarrer une partie</a>
+                <a class="nav-link" href="#">Démarrer une partie</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Continuer une partie</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Wiki</a>
+                <a class="nav-link" href="#">Wiki</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Forum</a>
+                <a class="nav-link" href="#">Forum</a>
             </li>
-          </ul>
-        </div>
+        </ul>
+    </div>
 </nav>
+</html>

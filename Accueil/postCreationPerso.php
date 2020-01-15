0a5f7f4b6dda07e3012e69fff2614c3cc31e3c36
Matthieu <guertener.matthieu@orange.fr>
2020-01-15 15:41:38 +0100

    creationPerso

--- Accueil/postCreationPerso.php
+++ Accueil/postCreationPerso.php
@@ -3,19 +3,12 @@
     require_once 'Model/allModel.php';
     require_once dirname(__DIR__).'\Accueil\Entity\Personnage.php';
 
-    if ($_POST['atq']+$_POST['vie']!=15){
-        $_SESSION['erreurPerso'] = 'Veuillez définir 15 points de caractéristiques au total !';
-        header('Location:creationPerso.php');
-        exit;
-    }
     $perso = new Personnage();
-    $perso->nom = $_POST['nom'];
+    $perso->nom = $_POST['name'];
     $perso->vie = $_POST['vie'];
     $perso->atq = $_POST['atq'];
 
     PersoModel::createPerso($_SESSION['userID']['id'], $perso);
-    var_dump($persoData);
-    die;
     header('Location:jeu.php');
     exit;
 ?>

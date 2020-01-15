4cb682210498c837d63a6681f214e91bc43fbb01
Matthieu <guertener.matthieu@orange.fr>
2020-01-15 15:28:01 +0100

    postCreationPerso

--- teste2e/Accueil/connexion.js
+++ teste2e/Accueil/connexion.js
@@ -2,6 +2,14 @@ module.exports = {
     'connexion': function(browser){
         browser
         .url('http://localhost/ProjetCODING/Accueil/')
-        .pause(10000)
+        .pause(3000)
+        .setValue('#username','Dramaticale')
+        .setValue('#password', 'DreamTheater64')
+        .click('body > div > div:nth-child(2) > div > form > div.bouton > button')
+        .pause(3000)
+        .waitForElementVisible('#navbarSupportedContent > ul > li.nav-item.deconnexion')
+        .expect.element('#navbarSupportedContent > ul > li.nav-item.deconnexion').text.to.contain('Bonjour Dramaticale !')
+        browser.pause(3000)
+
     }
 }
